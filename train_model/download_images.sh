c0dab61cc39900a3a4610be62a349e31e0b57cc0
Lukas Heimann <mail@liwde.de>
2019-06-01 17:11:28 +0200

    More Training Data!

--- train_model/download_images.sh
+++ train_model/download_images.sh
@@ -2,8 +2,8 @@
 
 rm -rf ./downloads/
 
-googleimagesdownload -k "art OR painting OR sculpture OR exhibition" -f jpg -i art -l 300 -cd /usr/lib/chromium-browser/chromedriver
-googleimagesdownload -k "trash OR rubbish" -f jpg -i trash -l 300 -cd /usr/lib/chromium-browser/chromedriver
+googleimagesdownload -k "art OR painting OR sculpture OR exhibition OR exhibit OR design OR kunstwerk" -f jpg -i art -l 3000 -cd /usr/lib/chromium-browser/chromedriver
+googleimagesdownload -k "trash OR rubbish OR junk OR abfall OR müll OR waste OR garbage OR scrap" -f jpg -i trash -l 3000 -cd /usr/lib/chromium-browser/chromedriver
 
 mogrify -resize 224x224\! -colorspace sRGB -format JPG ./downloads/*/*.*
 rm ./downloads/*/*.jpg
