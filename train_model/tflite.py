09da5d54d9f9e5bee077094e3b1bb900487d6e6b
Lukas Heimann <mail@liwde.de>
2019-06-01 16:03:48 +0200

    Create both float and quant tflite models

--- train_model/tflite.py
+++ train_model/tflite.py
@@ -8,4 +8,9 @@ model = tf.keras.models.load_model('model.h5')
 # Convert the model.
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
-open("model.tflite","wb").write(tflite_model)
+open("model_float.tflite","wb").write(tflite_model)
+
+converter_quant = tf.lite.TFLiteConverter.from_keras_model(model)
+converter_quant.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
+tflite_model_quant = converter_quant.convert()
+open("model_quant.tflite","wb").write(tflite_model_quant)
