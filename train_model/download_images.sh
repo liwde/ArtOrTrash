4fb0ab91fb0fb859d937c07be3d11bd19c628a6e
Lukas Heimann <mail@liwde.de>
2019-06-01 18:46:16 +0200

    Improve image download

--- train_model/download_images.sh
+++ train_model/download_images.sh
@@ -2,8 +2,15 @@
 
 rm -rf ./downloads/
 
-googleimagesdownload -k "art OR painting OR sculpture OR exhibition OR exhibit OR design OR kunstwerk" -f jpg -i art -l 3000 -cd /usr/lib/chromium-browser/chromedriver
-googleimagesdownload -k "trash OR rubbish OR junk OR abfall OR müll OR waste OR garbage OR scrap" -f jpg -i trash -l 3000 -cd /usr/lib/chromium-browser/chromedriver
+for i in "art" "painting" "sculpture" "exhibition" "exhibit" "design" "kunstwerk";
+do
+	googleimagesdownload -k "$i" -f jpg -i art -l 500 -cd /usr/lib/chromium-browser/chromedriver
+done
+
+for i in "trash" "rubbish" "junk" "waste" "garbage" "scrap" "abfall";
+do
+	googleimagesdownload -k "$i" -f jpg -i trash -l 500 -cd /usr/lib/chromium-browser/chromedriver
+done
 
 mogrify -resize 224x224\! -colorspace sRGB -format JPG ./downloads/*/*.*
 rm ./downloads/*/*.jpg
