3433a43d9715f2d721b080df35989b65cb3b7b27
Lukas Heimann <mail@liwde.de>
2019-06-03 21:48:43 +0200

    Add proper licensing notices

--- classifier_app/app/src/main/java/de/liwde/artortrash/tflite/ClassifierFloatMobileNet.java
+++ classifier_app/app/src/main/java/de/liwde/artortrash/tflite/ClassifierFloatMobileNet.java
@@ -1,4 +1,5 @@
 /* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+   Modified 2019 Lukas Heimann.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
